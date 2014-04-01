86969e43914aee3eec48a5db6d5133a606526bad
cdeange <christiand@carezone.com>
2014-03-31 22:50:08 -0400

    Fix issue with IAP dialog only showing once

--- marathon/src/main/java/com/deange/marathonapp/controller/BillingController.java
+++ marathon/src/main/java/com/deange/marathonapp/controller/BillingController.java
@@ -132,7 +132,7 @@ public final class BillingController {
                 }
             };
 
-            if (!mHelper.isSetupDone()) {
+            if (mHelper.isSetupDone()) {
                 resultProcess.run();
 
             } else {
